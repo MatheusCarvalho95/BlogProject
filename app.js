2b18884ce35192b4a7983fce3ea1bbf1ba3ec3f0
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2020-09-11 15:07:42 -0300

    updated listen settings

--- app.js
+++ app.js
@@ -74,6 +74,8 @@ post.save(function(err){
 })
 
 //Listen section
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server (re)started on port 3000");
-})
+let port = process.env.PORT;
+if (port == null || port== ""){
+  port = 3000;
+}
+app.listen(port)
