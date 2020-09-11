b1ffa9e36420d2459003d670a2ab1ce73874670d
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2020-09-11 15:00:01 -0300

    version 0.5

--- app.js
+++ app.js
@@ -3,10 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-const homeStartingContent = "This is a basic Blog project, here i'm making a daily journal and i'm gonna post some updates on my projects";
-const aboutContent = "I'm just a fullstack student.";
-const contactContent = "You can contact-me at: matheus_carvalho2@hotmail.com";
+const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -14,12 +12,34 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//Mongoose section
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser:true, useUnifiedTopology: true});
+
+const postSchema ={
+  title: String,
+  content: String
+};
+const Post = mongoose.model("Post", postSchema);
+
+
+
+
+
+
+
 //Pre variables section
-var posts = []
+
+const homeStartingContent = "This is a basic blog project, here i'm making a daily journal and i'm gonna post some updates on my projects";
+const aboutContent = "I'm just a fullstack student.";
+const contactContent = "You can contact-me at: matheus_carvalho2@hotmail.com";
+
+
 //Render pages section
 app.get("/",function(req,res){
-  res.render("home",{startC:homeStartingContent, postContent:posts}) 
-  
+  Post.find({}, function(err, posts){
+    res.render("home",{startC:homeStartingContent,postContent:posts})
+  })
 })
 app.get("/about", function(req,res){
   res.render("about",{aboutC:aboutContent})
@@ -30,17 +50,30 @@ app.get("/contact", function(req,res){
 app.get("/compose", function(req,res){
   res.render("compose")
 })
+
+app.get("/posts/:postId", function(req,res){
+    const requeredPostId = req.params.postId;
+    Post.findOne({_id:requeredPostId}, function(err,post){
+      res.render("post", {
+        title:post.title,
+        content:post.content
+      })
+    })
+  })
 //Post section
 app.post("/cp", function(req,res){
-  var newPost = {
+  const post = new Post ({
   title : req.body.titleContent,
   content : req.body.textContent
-}
-posts.push(newPost);
-res.redirect("/");
+})
+post.save(function(err){
+  if(!err){
+    res.redirect("/");
+  }
+});
 })
 
 //Listen section
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server (re)started on port 3000");
 })
