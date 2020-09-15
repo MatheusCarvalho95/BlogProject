a75b3efe69265296b167eda5817bf8b4d6e094bc
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2020-09-15 15:50:26 -0300

    Add authentication

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const app = express();
@@ -13,15 +16,50 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//Session section 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+//Passport initialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 //Mongoose section
 
-mongoose.connect("mongodb+srv://adminfordatabase:HRXEFNqNaiYL51AO@blackwork.bkdhb.gcp.mongodb.net/blogDB?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true});
-
+mongoose.connect(process.env.DATABASE_POSTS, {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+//Posts schema and models 
 const postSchema ={
   title: String,
   content: String
 };
 const Post = mongoose.model("Post", postSchema);
+
+//Users schema and models, config with passport
+const userSchema = new mongoose.Schema({
+  username : String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+//Passport
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 
 //Pre variables section
 
@@ -35,16 +73,21 @@ app.get("/",function(req,res){
   Post.find({}, function(err, posts){
     res.render("home",{startC:homeStartingContent,postContent:posts})
   })
-})
+});
 app.get("/about", function(req,res){
   res.render("about",{aboutC:aboutContent})
-})
+});
 app.get("/contact", function(req,res){
   res.render("contact",{contactC:contactContent})
-})
+});
 app.get("/compose", function(req,res){
-  res.render("compose")
-})
+    if(req.isAuthenticated()){
+      res.render('compose');
+    }else{
+      res.redirect("/login");
+    }
+  
+});
 
 app.get("/posts/:postId", function(req,res){
     const requeredPostId = req.params.postId;
@@ -54,18 +97,46 @@ app.get("/posts/:postId", function(req,res){
         content:post.content
       })
     })
+  });
+//Login route
+app.get('/login', function(req,res){
+  res.render('login')
+});
+
+app.post('/login', function(req,res){
+  const user = new User({
+    username: req.body.username,
+    password : req.body.password
+  });
+  req.login(user, function(err){
+    if(err){console.log(err)
+    }else{passport.authenticate("local")(req,res, function(){
+      res.redirect('/compose')
+    })}
   })
+
+});
+
+app.get('/logout', function(req,res){
+    req.logout();
+    res.redirect('/')
+})
+
 //Post section
 app.post("/cp", function(req,res){
+ if(req.isAuthenticated()){
   const post = new Post ({
-  title : req.body.titleContent,
-  content : req.body.textContent
-})
-post.save(function(err){
-  if(!err){
-    res.redirect("/");
-  }
-});
+    title : req.body.titleContent,
+    content : req.body.textContent
+  })
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+ }else{
+   res.redirect('/login')
+ }
 })
 //Functions to update and delete
 function editThePost(id,title,text){
@@ -87,15 +158,21 @@ app.post("/patch", function(req,res){
   const editId = req.body.postId;
   const editTitle = req.body.postTitle;
   const editText = req.body.textContent;
-  editThePost(editId, editTitle,editText);
-  res.send("Updated the post")
+  if(req.isAuthenticated()){
+    editThePost(editId, editTitle,editText);
+    res.send("Updated the post")
+  }
 });
 ;
 //Delete post
 app.post("/delete", function(req,res){
   const deleteId = req.body.deleteId;
+if(req.isAuthenticated()){
   deleteThePost(deleteId)
   res.send("Deleted the post")
+}else{
+  res.redirect('/login')
+}
 })
 
 //Listen section
@@ -103,4 +180,4 @@ let port = process.env.PORT;
 if (port == null || port== ""){
   port = 3000;
 }
-app.listen(port)
+app.listen(port,function(){console.log("server started")});
